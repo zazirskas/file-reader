0b401fb2bf7fbf2e094cd686cd3257181aa83c5e
Thiago Zazirskas <zazirskas@hotmail.com>
2024-07-05 13:23:46 +0300

    Removing dist folder

--- dist/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var cacheFilePath = "".concat(process.env.CACHE_FOLDER_PATH, "/").concat(process.env.STATE_FILENAME);
-var _default = exports["default"] = cacheFilePath;
